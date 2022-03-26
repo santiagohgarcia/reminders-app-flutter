24a3b7dfb2419be7cb994ff6e1de2783fa9d7fa8
Santiago Garcia <shgarcia@tgvamericas.net>
2022-03-26 18:09:09 -0400

    new theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,25 +1,37 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 var appTheme = ThemeData(
-  fontFamily: GoogleFonts.nunito().fontFamily,
-  bottomAppBarTheme: const BottomAppBarTheme(
-    color: Colors.black87,
-  ),
-  brightness: Brightness.dark,
-  textTheme: const TextTheme(
-    bodyText1: TextStyle(fontSize: 18),
-    bodyText2: TextStyle(fontSize: 16),
-    button: TextStyle(
-      letterSpacing: 1.5,
-      fontWeight: FontWeight.bold,
+    brightness: Brightness.light,
+    visualDensity: VisualDensity(vertical: 0.5, horizontal: 0.5),
+    primarySwatch: MaterialColor(
+      0xffE1E2E1,
+      <int, Color>{
+        50: Color.fromRGBO(225, 226, 225, .1),
+        100: Color.fromRGBO(225, 226, 225, .2),
+        200: Color.fromRGBO(225, 226, 225, .3),
+        300: Color.fromRGBO(225, 226, 225, .4),
+        400: Color.fromRGBO(225, 226, 225, .5),
+        500: Color.fromRGBO(225, 226, 225, .6),
+        600: Color.fromRGBO(225, 226, 225, .7),
+        700: Color.fromRGBO(225, 226, 225, .8),
+        800: Color.fromRGBO(225, 226, 225, .9),
+        900: Color.fromRGBO(225, 226, 225, 1),
+      },
     ),
-    headline1: TextStyle(
-      fontWeight: FontWeight.bold,
-    ),
-    subtitle1: TextStyle(
-      color: Colors.grey,
-    ),
-  ),
-  buttonTheme: const ButtonThemeData(),
-);
+    primaryColor: Color(0xffE1E2E1),
+    primaryColorBrightness: Brightness.light,
+    primaryColorLight: Color(0xffE1E2E1),
+    primaryColorDark: Color(0xffE1E2E1),
+    canvasColor: Color(0xffE1E2E1),
+    accentColor: Color(0xff3949ab),
+    accentColorBrightness: Brightness.light,
+    scaffoldBackgroundColor: Color(0xffB5BFD3),
+    bottomAppBarColor: Color(0xff6D42CE),
+    cardColor: Colors.white,
+    dividerColor: Color(0x1f6D42CE),
+    focusColor: Color(0x1aF5E0C3));
+
+
+
